abd421b39fbad93e7b1fda94ac17b046c306b988
harellevy <harellevy@gmail.com>
2019-06-15 20:08:34 +0300

    - a small msg error reset fix

--- apps/mini-imdb/src/app/features/search-results/search-results.component.ts
+++ apps/mini-imdb/src/app/features/search-results/search-results.component.ts
@@ -12,9 +12,9 @@ import { MovieDetailsComponent } from '../movie-details/movie-details.component'
 export class SearchResultsComponent implements OnInit {
   searchResults: Partial<IMovieSearchResult> = {};
   searchInput: string;
-  page: number = 1; // 1 is the default in api
+  page = 1; // 1 is the default in api
   params: ISearchParams = {}; // 1 is the default in api
-  validSearchInput = true;
+  validSearchInput = false;
   constructor(
     private searchService: SearchService,
     private modalService: MatDialog
@@ -30,17 +30,27 @@ export class SearchResultsComponent implements OnInit {
       });
 
     dialogRef.afterClosed().subscribe(result => {
+      console.log(result);
       // if need to set something after modal closed
     });
   }
 
   search(params: ISearchParams) {
     // don't run if search query less then 3 chars.
-    if (params.s && params.s.length < 3 && !params.y) {
+    if (!params.s || params.s.length === 0) {
+      // reset err msg
+      this.validSearchInput = false;
+      this.searchResults = null;
       return;
     }
+    if (params.s && params.s.length < 3 && !params.y) {
+      // just return;
+      return;
+    }
+    // reset list to be nothing, should be populated soon if there are results
     this.searchResults = {Search: []};
     this.page = 1; // reset page to default
+    // populate params (older params, page, and params passed)
     this.params = {...this.params, ...params, ...{page: this.page}};
     this.searchService.find(this.params).subscribe((searchResult: IMovieSearchResult) => {
       if (searchResult.Response !== 'True') {
